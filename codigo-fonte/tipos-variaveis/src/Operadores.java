83faf3ed09f7cac87a9c33e03b6667592cdc6fe4
Edivaldo <nenebsgi@hotmail.com>
2023-04-01 14:01:12 -0300

    Novo commit

--- codigo-fonte/tipos-variaveis/src/Operadores.java
+++ codigo-fonte/tipos-variaveis/src/Operadores.java
@@ -1,5 +1,10 @@
 public class Operadores {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        
+        String nome = "Edivaldo";
+        String sobreNome = "Aparecido Dias";
+        System.out.println(nome + sobreNome);
     }
 }
